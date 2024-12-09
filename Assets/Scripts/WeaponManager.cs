1e09e8ae936356a34098d0d01a4eaf45b31b2ac3
Kevin DeCara <kevindecara@MacBookAir.fios-router.home>
2024-12-09 04:06:26 -0500

    music and final changes

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
     public GameObject playerCam;
-    public float range = 1000f; //range of weapon
+    public float range = 1000f;
     public float damage = 100f;
     public Animator playerAnimator;
+
+    [SerializeField]
+    private AudioSource weaponAudioSource; // Reference in Inspector
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Make sure weaponAudioSource is assigned either via Inspector or via GetComponent<>():
+        if (weaponAudioSource == null)
+        {
+            weaponAudioSource = GetComponent<AudioSource>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerAnimator.GetBool("isShooting"))
+        if (playerAnimator.GetBool("isShooting"))
         {
             playerAnimator.SetBool("isShooting", false);
         }
 
-        if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
@@ -32,17 +39,22 @@ public class WeaponManager : MonoBehaviour
     void Shoot()
     {
         playerAnimator.SetBool("isShooting", true);
+        
         RaycastHit hit;
-        if(Physics.Raycast(playerCam.transform.position, transform.forward, out hit, range))
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
         {
             EnemyManager enemyManager = hit.transform.GetComponentInParent<EnemyManager>();
-            if(enemyManager != null) //if not null we hit an enemy
+            if (enemyManager != null)
             {
                 Debug.Log(hit.transform.name);
                 enemyManager.Hit(damage);
             }
         }
 
-
+        // Play the shooting sound
+        if (weaponAudioSource != null)
+        {
+            weaponAudioSource.Play();
+        }
     }
 }
